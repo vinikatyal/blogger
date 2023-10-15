550056ca51ada7e1fb98f8e8c0ef49df3da110df
Vini Katyal <vinikatyal90@gmail.com>
2023-10-15 13:21:58 +0530

    chore: change env variables

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 
-const MONGO_URI: string = process.env.MONGODB_URI || ''
+const MONGO_URI = process.env.MONGODB_URI as string; // your mongodb connection string
 
 const connectMongoDB = async () => {
   try {
